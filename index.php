e4e8f44b66c8542954df8a535c6d80149d3c8661
Djordje Milosevic <djodjolos@gmail.com>
2020-06-26 17:29:33 +0200

    11

--- index.php
+++ index.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CD Example</title>
+
 </head>
 <body>
     <h1>Works 12345</h1>
+    <h3><?php echo "I php radi!" ?> </h3>
 </body>
 </html>
